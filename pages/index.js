987eb43ed5d0802586959fe41ac580e0c45c226c
William <willjamesr@gmail.com>
2023-02-08 14:15:47 +1100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+import {initMongoose} from "../lib/mongoose";
+import {findAllProducts} from "./api/products";
+
+export default function Home({products}) {
+  const [phrase, setPhrase] = useState('');
+
+  const categoriesNames = [...new Set(products.map(p => p.category))];
+
+  if (phrase) {
+    products = products.filter(p => p.name.toLowerCase().includes(phrase));
+  }
+
+  return (
+    <div className="p-5">
+      <input 
+        value ={phrase} 
+        onChange={e => setPhrase(e.target.value)} 
+        type="text" 
+        placeholder="Search for Products" 
+        className="bg-gray-100 w-full py-2 px-4 rounded-xl"
+      />
+      <div>
+        {categoriesNames.map(categoryName => (
+          <div key={categoryName}>
+            {products.find(p => p.category === categoryName) && (
+             
+              <div>
+                <h2 className="text-2xl capitalize">{categoryName}</h2>
+                
+                <div className="flex -mx-5 overflow-x-scroll snap-start scrollbar-hide">
+                  {products.filter(p => p.category === categoryName).map(productInfo => (
+                
+                <div key={productInfo._id} className="px-5">
+                <Product {...productInfo} />
+                </div>
+                
+                ))}
+                </div>
+              </div>
+              )}
+            
+          </div>
+          ))}
+      </div>
+
+    <footer className="">
+
+    </footer>
+
+    </div>
+  )
+}
+
+
+export async function getServerSideProps() {
+  await initMongoose();
+  const products = await findAllProducts();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+}
